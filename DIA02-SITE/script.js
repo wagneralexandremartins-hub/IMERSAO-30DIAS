9f9909af23e3ec3c17d29d93d40300745e08c746
wagner martnis <wagneralexandre.martins@gmail.com>
2026-02-06 16:36:37 -0300

    Delete DIA02-SITE directory

--- DIA02-SITE/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("ano").textContent = new Date().getFullYear();
-
-const btn = document.getElementById("btn");
-const msg = document.getElementById("msg");
-
-btn.addEventListener("click", () => {
-  msg.textContent = "🔥 Dia 02 concluído com sucesso!";
-});
